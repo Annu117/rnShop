3ea1289795dd5490937f7ce0c6b6977086c5a9e7
Annu Kumari <108427028+Annu117@users.noreply.github.com>
2025-02-08 20:34:22 +0530

    Update Auth.js

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,32 +1,4 @@
-// const jwt = require("jsonwebtoken");
-// const asyncHandler = require("express-async-handler");
-// const User = require("../models/User");
 
-// const protect = asyncHandler(async (req, res, next) => {
-//   let token;
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     try {
-//       token = req.headers.authorization.split(" ")[1];
-//       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-//       req.user = await User.findById(decodedToken.id).select("-password");
-//       next();
-//     } catch (err) {
-//       console.error(err);
-//       res.status(401).json({ message: "Not authorized" });
-//     }
-//   }
-//   if (!token) {
-//     res.status(401);
-//     throw new Error("Not authorized..!");
-//   }
-// });
-
-// module.exports = protect;
-
-// api/middleware/Auth.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
